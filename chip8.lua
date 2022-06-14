53688f855bf6a64b6a881f868deaf0c0bb96cafe
Larry Xu <hilarryxu@gmail.com>
2022-06-14 20:49:15 +0800

    feat: add chip8_mt.get_next_opcode

--- chip8.lua
+++ chip8.lua
@@ -1,6 +1,10 @@
 local ffi = require "ffi"
+local bit = require "bit"
 
 local ffi_new, ffi_cast, ffi_copy = ffi.new, ffi.cast, ffi.copy
+local bnot = bit.bnot
+local band, bor, bxor = bit.band, bit.bor, bit.bxor
+local lshift, rshift = bit.lshift, bit.rshift
 
 ffi.cdef [[
 typedef struct chip8 {
@@ -18,6 +22,8 @@ typedef struct chip8 {
   uint16_t *stack;  // ram[0xea0 ~ 0xeff]
 
   uint8_t keystate[16];
+
+  uint16_t rom_size;  // ROM size in bytes
 } chip8_t;
 ]]
 
@@ -34,6 +40,14 @@ end
 
 local chip8_mt = aux.class()
 
+function chip8_mt.get_next_opcode(vm)
+  assert(vm.pc >= ENTRY_BASE and vm.pc < ENTRY_BASE + vm.rom_size)
+
+  local opcode = bor(lshift(vm.ram[vm.pc], 8), vm.ram[vm.pc + 1])
+  vm.pc = vm.pc + 2
+  return opcode
+end
+
 ffi.metatype("chip8_t", chip8_mt)
 
 local function Chip8(rom, rom_size)
@@ -42,6 +56,7 @@ local function Chip8(rom, rom_size)
   if rom ~= nil then
     rom_size = rom_size or #rom
     ffi_copy(vm.ram + ENTRY_BASE, rom, rom_size)
+    vm.rom_size = rom_size
   end
 
   vm.screen = vm.ram + 0xf00
@@ -57,10 +72,15 @@ _M.Chip8 = Chip8
 local mod_name = ...
 local argc = #arg
 if mod_name == nil then
-  local vm = Chip8()
+  local vm = Chip8 "\x00\xe0"
+  local opcode
 
   assert(vm.I == 0, "I == 0")
   assert(vm.pc == ENTRY_BASE, "vm.pc == ENTRY_BASE")
+
+  opcode = vm:get_next_opcode()
+  assert(opcode == 0x00e0)
+  assert(vm.pc == ENTRY_BASE + 2)
 end
 
 return _M
